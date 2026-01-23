9a9b81883279039be46cd8483b7c47dfc7fccdd5
Faysal <k.hasanfaysal@gmail.com>
2026-01-23 07:47:54 +0600

    js first module finished

---
+++ javascript/note.md
@@ -0,0 +1,21 @@
+# Variable:
+
+- String
+- boolian
+-
+
+# Name is Case Sensetive
+
+var address
+var address
+var addRess
+
+# Convert a String into a Intiger
+
+- perseInt
+
+## JavaScript -> JIT Compiled [Just In Time]
+
+## -> High Level Lang
+
+## -> Interpret
--- javascript/variable.js
+++ javascript/variable.js
@@ -1,2 +1,45 @@
-var age;
-console.log(age);
+var age = 25;
+age = 30;
+var name = "John";
+name = "Faysal";
+// console.log(age);
+// console.log(name);
+
+let isRich = true;
+// console.log(isRich);
+// console.log(typeof isRich);
+
+//  Name is Case Sensetive
+
+var address;
+var address;
+var addRess;
+
+// Convert String to Number
+var applePrice = 10;
+var mangoPrice = "15";
+// mangoPrice = parseInt(mangoPrice);
+// mangoPrice = Number(mangoPrice);
+applePrice = parseFloat("22.2");
+// console.log(applePrice);
+
+// Exception Things
+var a = 0.1;
+var b = 0.2;
+var total = a + b;
+// console.log(total);
+// console.log(total.toFixed(2));
+
+// concatanation
+var firstName = "Faysal";
+var lastName = "Khan";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(50 + "20");
+console.log("50" + 230);
+
+// Subtraction
+var gPrice = 50;
+var pPrice = "30";
+var diff = gPrice - pPrice;
+console.log(diff);
