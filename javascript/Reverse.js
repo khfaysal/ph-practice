4b9d184567dad4fab4a29ba1e0008c57833f61e1
Faysal <k.hasanfaysal@gmail.com>
2026-01-30 02:41:05 +0600

    string reverse

---
+++ javascript/Reverse.js
@@ -0,0 +1,8 @@
+let motto = "I will learn js properly Insha'Allah";
+
+let revmotto = motto.split("").reverse().join(" ");
+console.log(revmotto);
+
+//split("")-> split all the letter with space.
+//reverse()-> do the reverse
+//join("")-> join all the words with proper space.
