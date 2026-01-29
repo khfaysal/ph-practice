1fe677adb0956c24c33c0676bc3e22da04379618
Faysal <k.hasanfaysal@gmail.com>
2026-01-29 15:39:56 +0600

    addition only odds

--- javascript/for-of.js
+++ javascript/for-of.js
@@ -1,7 +1,6 @@
 const fruits = ["apple", "banana", "cherry"];
 
-for(const fruit of fruits){
-    console.log(fruit);
-    console.log("I wanna eat");
-    
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("I wanna eat");
 }
---
+++ javascript/while.js
@@ -0,0 +1,35 @@
+// Task-1:
+// let num = 0;
+// while (num < 10) {
+//   console.log("loopin....", num);
+//   num += 2;
+// }
+
+// Task-2:
+// let ini = 1;
+// while (ini <= 10) {
+//   console.log(ini);
+//   ini += 1;
+// }
+
+// Task-3: count 1 to 10
+// let value = 1;
+// let sum = 0;
+// while (value <= 10) {
+//   sum = sum + value;
+//   value++;
+// }
+// console.log(sum);
+
+// Task-4: count 1 to 10( odds only)
+let v = 1;
+let sumofodd = 0;
+while (v <= 10) {
+  console.log("v:", v);
+  sumofodd = sumofodd + v;
+  v += 2;
+  console.log("------");
+  console.log(sumofodd);
+}
+console.log("------");
+console.log(sumofodd);
