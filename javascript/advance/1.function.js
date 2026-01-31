e98090fbd2d9fc47cad037712dd02d339f46bc71
Faysal <k.hasanfaysal@gmail.com>
2026-02-01 00:56:23 +0600

    function in js

---
+++ javascript/advance/1.function.js
@@ -0,0 +1,14 @@
+//Function Declaration..
+function OnOff() {
+  // console.log("Go towards the door.");
+  // console.log("Click the switch to off.");
+}
+
+//Function Call:
+// OnOff();
+
+function add(a, b) {
+  const total = a + b;
+  console.log("Total some of those numbers are: ", total);
+}
+add(2, 3);
