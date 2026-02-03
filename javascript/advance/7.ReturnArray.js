7cd8c560fc38bcd9ce58d347b02539dbca6f9132
Faysal <k.hasanfaysal@gmail.com>
2026-02-03 12:49:30 +0600

    module 18 started

---
+++ javascript/advance/7.ReturnArray.js
@@ -0,0 +1,14 @@
+function returnArr(arr) {
+  let odd = [];
+  for (const num of arr) {
+    if (num % 2 != 0) {
+      console.log(num);
+      odd.push(num);
+    }
+  }
+  return odd;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const ShowOdd = returnArr(arr);
+console.log(ShowOdd);
