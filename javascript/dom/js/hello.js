ca0e25174e2c89f38c9f956c6271fdbf7a210c1a
Faysal <k.hasanfaysal@gmail.com>
2026-02-17 11:46:55 +0600

    dom manipulaton

--- javascript/dom/js/hello.js
+++ javascript/dom/js/hello.js
@@ -1,1 +1,39 @@
-// console.log("Hello World..");
+console.log("something biggest happening here...");
+const liCollection = document.getElementsByTagName("li");
+console.log(liCollection);
+for (const li of liCollection) {
+  console.log(li);
+}
+
+const foodt = document.getElementById("food-tittle");
+//get-id
+console.log(foodt.innerText);
+foodt.innerText = "food that make me feel awesome";
+//get-id
+const jalSet = document.getElementById("jalS");
+console.log(jalSet.innerText);
+
+//get-Class
+const prioKhabar = document.getElementsByClassName("prio");
+console.log(prioKhabar);
+for (const khaun of prioKhabar) {
+  console.log(khaun.innerText);
+}
+//advance CSS selector:
+const adv = document.querySelectorAll(".dom span");
+console.log(adv);
+for (const sp of adv) {
+  console.log(sp.innerText);
+}
+//Change css using js ***
+const changeColor = (foodt.style.color = "red");
+foodt.style.backgroundColor = "aqua";
+document.getElementById("jalS").style.color = "red";
+foodt.classList.add("bg-red");
+foodt.classList.remove("bg-red");
+
+const sections = document.querySelectorAll("section");
+for (sec of sections) {
+  console.log(sec);
+  sec.style.backgroundColor = "skyblue";
+}
---
+++ javascript/dom/note.md
@@ -0,0 +1,34 @@
+- getElementById -> thakle
+- getElementById -> na thakle -> null
+
+- getElementByClassName -> thakle -> array like Object == html collection
+- getElementByClassName -> na thakle -> empty array 
+
+* querySelectorAll -> it will search all
+* querySelector -> it will search only the first one
+
+
+### difference: html collection, node list, null, undefined, 
+
+
+
+
+
+* dom traversing -> way of finding element from the document tree using JS.
+
+* querySelector -> Google of DOM
+
+*
+
+
+
+## Leason::
+document.
+* getELementByTagName
+* getELementById
+* getELementByClassName
+* querySelectorAll
+* querySelector
+* change css using js
+* getAtribute
+* setAtribute
