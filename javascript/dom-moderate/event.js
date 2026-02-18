21192856bae2cab0abcf3a752478c81b45eeff05
Faysal <k.hasanfaysal@gmail.com>
2026-02-19 05:35:34 +0600

    4 types of onclick things

--- javascript/dom-moderate/event.js
+++ javascript/dom-moderate/event.js
@@ -1,1 +1,27 @@
-console.log("event is working!");
+// Type-2
+function mY() {
+  document.body.style.backgroundColor = "yellow";
+}
+function mR() {
+  document.body.style.backgroundColor = "red";
+}
+function mG() {
+  document.body.style.backgroundColor = "green";
+}
+function mP() {
+  document.body.style.backgroundColor = "purple";
+}
+function mA() {
+  document.body.style.backgroundColor = "aqua";
+}
+
+// Type-3: another way to call by 'Id'
+const btnGreen = document.getElementById("btn-green");
+btnGreen.onclick = mG; // Assign the function reference, NOT call it
+
+const btnPurple = document.getElementById("btn-purple");
+btnPurple.onclick = mP;
+
+// Type-4: addEventListener
+const AQ = document.getElementById("make-aqua").addEventListener("click", mA);
+
