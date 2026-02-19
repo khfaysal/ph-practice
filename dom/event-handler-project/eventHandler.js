f3b285322a1aa9565a0c6f4fdf0d5ade27018302
Faysal <k.hasanfaysal@gmail.com>
2026-02-20 01:52:30 +0600

    dom manipulation project done

--- dom/event-handler-project/eventHandler.js
+++ dom/event-handler-project/eventHandler.js
@@ -1,6 +1,6 @@
 //Faysal
 document.getElementById("faysal").addEventListener("click", function () {
-  const bgColor = (document.body.style.backgroundColor = "yellow");
+  const bgColor = (document.body.style.backgroundColor = "green");
   const nameChange = document.getElementById("fayName");
   nameChange.innerText = "Bhalo Manush";
   nameChange.style.fontSize = "2rem";
@@ -8,9 +8,89 @@ document.getElementById("faysal").addEventListener("click", function () {
 });
 //Meherab
 document.getElementById("meherab").addEventListener("click", function () {
-  const bgColor = (document.body.style.backgroundColor = "blue");
+  const bgColor = (document.body.style.backgroundColor = "yellow");
   const nameChange = document.getElementById("mehName");
-  nameChange.innerText = "Fauwa lok";
+  nameChange.innerText = "";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Irfan
+document.getElementById("irfan").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "blue");
+  const nameChange = document.getElementById("irfName");
+  nameChange.innerText = "Player Bhai";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Saim
+document.getElementById("saim").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "red");
+  const nameChange = document.getElementById("saiName");
+  nameChange.innerText = "Gyan Baba";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Pra
+document.getElementById("pra").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "Brown");
+  const nameChange = document.getElementById("praName");
+  nameChange.innerText = "Gan Baba";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Jakia
+document.getElementById("jak").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "hotpink");
+  const nameChange = document.getElementById("jakName");
+  nameChange.innerText = "Gan Baba";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Subrina
+document.getElementById("sub").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "pink");
+  const nameChange = document.getElementById("subName");
+  nameChange.innerText = "Ajke amar mon bhalo nei";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Maruf
+document.getElementById("mar").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "Purple");
+  const nameChange = document.getElementById("marName");
+  nameChange.innerText = "Bus dhorte hobe!";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Nadim
+document.getElementById("nad").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "Indigo");
+  const nameChange = document.getElementById("nadName");
+  nameChange.innerText = "Age basay gele age chakri pabo!";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Fardin
+document.getElementById("far").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "Violet");
+  const nameChange = document.getElementById("farName");
+  nameChange.innerText = "Sada Mal";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Arian
+document.getElementById("ari").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "aqua");
+  const nameChange = document.getElementById("ariName");
+  nameChange.innerText = "Amar barite vut ache";
+  nameChange.style.fontSize = "2rem";
+  nameChange.style.textAlign = "center";
+});
+//Ovi
+document.getElementById("ovi").addEventListener("click", function () {
+  const bgColor = (document.body.style.backgroundColor = "Coral");
+  const nameChange = document.getElementById("oviName");
+  nameChange.innerText = "Jaura Beda";
   nameChange.style.fontSize = "2rem";
   nameChange.style.textAlign = "center";
 });
