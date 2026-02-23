81a00f07908dfda516156ec80e30008bcc1517f2
Faysal <k.hasanfaysal@gmail.com>
2026-02-24 02:29:30 +0600

    assignment-4 half complete

---
+++ javascript/practice/blockScope.js
@@ -0,0 +1,9 @@
+function lett() {
+  if (true) {
+    let x = 10;
+  }
+  console.log(x);
+}
+lett();
+
+//x is not define
---
+++ javascript/practice/funcScope.js
@@ -0,0 +1,8 @@
+function varr() {
+  if (true) {
+    var x = 10;
+  }
+  return x;
+}
+const result = varr();
+console.log(result);
