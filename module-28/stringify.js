029701447546eabe7241e6974436c70a119863a6
Faysal <k.hasanfaysal@gmail.com>
2026-03-06 15:51:33 +0600

    api fetch

---
+++ module-28/stringify.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+
+x = CSSFontFeatureValuesRule;
+console.log(person, typeof person);
+
+const name = JSON.stringify(person);
+console.log(name, typeof name);
